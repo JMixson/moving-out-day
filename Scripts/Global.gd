d060e64b5be689d186ee877ee24be10d21f31f9b
JMixson <jmixson1179@gmail.com>
2022-04-17 10:06:36 -0700

    Add variable for box amount

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -3,3 +3,5 @@ extends Node
 var box_values = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550]
 
 var price = 0
+
+var box_num = 0
--- Scripts/Level 1.gd
+++ Scripts/Level 1.gd
@@ -5,9 +5,13 @@ func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
+func _process(delta):
+	if (Global.box_num == 0):
+		$Ending.visible = true
 #	pass
 
 func _on_Door_body_entered(body):
 	if (body.get_name() == "RigidBody2D"):
 		body.queue_free()
+		Global.box_num -= 1
+		print("Boxes: " + str(Global.box_num))
