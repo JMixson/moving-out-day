6470a2fb9bda2c4466dee8eef524925f188f22ff
JMixson <jmixson1179@gmail.com>
2022-04-16 19:19:15 -0700

    Rename label

--- Scripts/Top UI.gd
+++ Scripts/Top UI.gd
@@ -13,4 +13,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$Price.text = "$" + str(Global.price);
+	$Total.text = "$" + str(Global.price);
