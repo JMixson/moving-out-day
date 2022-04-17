1dc523a0f82a4b9f8678c0b9ae463b92e775024c
JMixson <jmixson1179@gmail.com>
2022-04-17 08:56:02 -0700

    Create Pause scene

---
+++ Scripts/Pause.gd
@@ -0,0 +1,34 @@
+extends Control
+
+var is_paused = false
+
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		if !get_tree().paused:
+			visible = true
+			pause()
+		else:
+			visible = false
+			resume()
+	
+func pause():
+	get_tree().set_deferred("paused", true)
+	
+func resume():
+	get_tree().set_deferred("paused", false)
+
+func _on_Continue_Btn_pressed():
+	visible = false
+	resume()
+
+func _on_Restart_Btn_pressed():
+	Global.price = 0
+	get_tree().change_scene("res://Scenes/Level 1.tscn")
+	resume()
+
+func _on_Menu_Btn_pressed():
+	Global.price = 0
+	get_tree().change_scene("res://Scenes/Main Menu.tscn")
+
+func _on_Quit_Btn_pressed():
+	get_tree().quit()
