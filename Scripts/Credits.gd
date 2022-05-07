d5050a9d9364f02ae9fa318bc1aff9b596e7f3ba
JMixson <jmixson1179@gmail.com>
2022-05-07 02:54:27 -0700

    Change Quit to Restart

--- Scripts/Credits.gd
+++ Scripts/Credits.gd
@@ -1,9 +1,14 @@
 extends Control
 
+func resume():
+	get_tree().set_deferred("paused", false)
+
 func _on_Menu_Btn_pressed():
 	get_tree().change_scene("res://Scenes/Main Menu.tscn")
 
-func _on_Q_Btn_pressed():
-	get_tree().quit()
+func _on_R_Btn_pressed():
+	Global.price = 0
+	get_tree().change_scene("res://Scenes/Level 1.tscn")
+	resume()
 
 
