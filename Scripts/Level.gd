8583cee54fb019a549a350d82aa734ccce89de8e
JMixson <jmixson1179@gmail.com>
2022-05-28 01:33:39 -0700

    Edit dialog text

--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -13,6 +13,9 @@ func _ready():
 func _process(delta):
 	if (Global.box_num == 0):
 		$Menus/Ending.visible = true
+		
+	if (Global.level_num > 1):
+		$Dialog_Panel/Dialog_Label.text = "Boxes Left: " + str(Global.box_num)
 
 func spawn_boxes(num):
 	for i in range(num):
@@ -27,6 +30,7 @@ func _on_Door_body_entered(body):
 	if (body.get_name() == "RigidBody2D"):
 		body.queue_free()
 		Global.box_num -= 1
+		$Dialog_Panel/Dialog_Label.text = "Boxes Left: " + str(Global.box_num)
 		print("Boxes: " + str(Global.box_num))
 
 func _on_Pause_Btn_pressed():
