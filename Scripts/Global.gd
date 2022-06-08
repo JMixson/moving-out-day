f9830a21e2ff4a4bed9842b9f50b3d8b24229ef8
JMixson <jmixson1179@gmail.com>
2022-06-07 17:36:29 -0700

    Update global variables and functions

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -1,19 +1,28 @@
 extends Node
 
-var box_values = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550]
+#var box_values = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550]
 
 var price = 0
 
 var box_num = 0
-
 var level_num = 1
 
 var drop_sound_on = true
 
 var game_end = false
 
-func restart_settings():
+var total_boxes = 0
+var total_money = 0
+
+func restart_level_settings():
+	Global.price = 0
+	Global.box_num = 0
+	Global.game_end = false
+
+func restart_game_settings():
 	Global.price = 0
 	Global.box_num = 0
 	Global.level_num = 1
+	Global.total_boxes = 0
+	Global.total_money = 0
 	Global.game_end = false
